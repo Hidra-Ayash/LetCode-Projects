cbb1e5db123232815a39ce903470dd27251f368e
Hidra Ayash <hydraysh123@gmail.com>
2024-04-25 20:57:31 +0300

    ADD The Solution og Merged Two List By using Linked List Pointer with img Example

---
+++ Merge Two Sorted List.js
@@ -0,0 +1,35 @@
+// You are given the heads of two sorted linked lists list1 and list2.
+
+// Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+// Return the head of the merged linked list.
+//Example (merge_ex1.jpg)
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (list1, list2) {
+  let head = new ListNode();
+  let cur = head;
+  while (list1 != null && list2 != null) {
+    if (list1.val < list2.val) {
+      cur.next = list1;
+      list1 = list1.next;
+    } else {
+      cur.next = list2;
+      list2 = list2.next;
+    }
+    cur = cur.next;
+  }
+  cur.next = list1 || list2;
+  return head.next;
+};
