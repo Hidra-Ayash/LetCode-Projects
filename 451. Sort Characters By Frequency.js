6f8fb4f23aceee0343325ab76b7a3be38027a992
Hidra Ayash <hydraysh123@gmail.com>
2024-04-29 15:39:11 +0300

    ADD The Solution of         451. Sort Characters By Frequency.js

---
+++ 451. Sort Characters By Frequency.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var frequencySort = function (s) {
+  let res = [];
+
+  for (let i = 0; i < s.length; i++) {
+    res.push(s.charCodeAt(i));
+  }
+  const ansObj = {};
+  for (let num of res) {
+    ansObj[num] = (ansObj[num] || 0) + 1;
+  }
+
+  let ans = {};
+  let ansRes = [];
+  ans = Object.entries(ansObj).sort((a, b) => b[1] - a[1]);
+  for (let i = 0; i < ans.length; i++) {
+    let freq = ans[i];
+    for (let i = 0; i < freq[1]; i++) {
+      ansRes.push(freq[0]);
+    }
+  }
+  return String.fromCharCode(...ansRes);
+};
+
+console.log(frequencySort("tree"));
