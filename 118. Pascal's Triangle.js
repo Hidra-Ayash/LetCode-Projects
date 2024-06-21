738059df41c8b39926836cebf3def002178eab58
Hidra Ayash <hydraysh123@gmail.com>
2024-06-22 01:05:52 +0300

    ADD The Solution of Pascal Probleme By Calculate the previous number and add one in the front and end of arrayPascal

--- 118. Pascal's Triangle.js
+++ 118. Pascal's Triangle.js
@@ -1,18 +1,36 @@
 /**
- * @param {number} numRows
- * @return {number[][]}
+ *
+ * @param {Number} numRow
+ * @param {[][]}resPascal
+ * @param {[]} newRow
  */
-var generate = function (numRows) {
-  let arrPascal = [[1]];
-  for (let i = 1; i < numRows; i++) {
-    let previousArray = arrPascal[arrPascal.length - 1];
-    let newArrayPascal = [1];
-    for (let j = 0; j < previousArray.length - 1; j++) {
-      newArrayPascal.push(previousArray[j] + previousArray[j + 1]);
+//Pascal Triangle And How To Draw This ???
+//Here Are The Code of Triangle in JavaScript Language
+//Space Complexity is : O(n^2)
+
+function createPascaslTraingleByNumRow(numRow) {
+  let resPascal = [[1]]; //Create The Array of Pascal Triangle
+  for (let i = 0; i < numRow; i++) {
+    const newRow = [1]; //Create A new Row Of Pascal Triangle
+    let elm = resPascal[i]; //1D Element of Pascal Triangle
+    for (let j = 0; j < elm.length - 1; j++) {
+      newRow.push(elm[j] + elm[j + 1]); //Add the sum of previous Two Number
     }
-    newArrayPascal.push(1);
-    arrPascal.push(newArrayPascal);
+    newRow.push(1); //Add the One In The End
+    resPascal.push(newRow); //Add The New Row Of Triangle in The Result Array
   }
-  return arrPascal;
-};
-console.log(generate(6));
+  return resPascal;
+}
+console.log(`The Result is :`, createPascaslTraingleByNumRow(5));
+
+//The Result is :
+/**
+                     [
+                        [ 1 ], 
+                       [ 1, 1 ],  
+                     [ 1, 2, 1 ],  
+                    [ 1, 3, 3, 1 ],       
+                   [ 1, 4, 6, 4, 1 ],    
+                 [ 1, 5, 10, 10, 5, 1 ]
+                     ]
+ */
